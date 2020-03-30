8d4d9d31691bfcc145052b9d639ba58d43e07899
Rogério Fontes <fontestz@gmail.com>
2020-03-30 20:35:21 -0300

    crud da API Rest

--- src/main/java/br/com/unipac/apitrabalhos/recoursos/TrabalhoRecursos.java
+++ src/main/java/br/com/unipac/apitrabalhos/recoursos/TrabalhoRecursos.java
@@ -1,9 +1,14 @@
 package br.com.unipac.apitrabalhos.recoursos;
 
 import br.com.unipac.apitrabalhos.model.domain.Trabalho;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.lang.reflect.Array;
+import java.net.URI;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -13,25 +18,50 @@ import java.util.List;
 public class TrabalhoRecursos {
     List<Trabalho> trabalhoList = new ArrayList<>();
 
-    @GetMapping("/id")
-    public Trabalho getTrabalho(){
-        Trabalho trabalho = new Trabalho();
-        trabalho.setId(1l);
-        trabalho.setData(new Date());
-        trabalho.setProfessor("Daniel");
-        trabalho.setTituloDoTrabalho("Criar API web");
+    @GetMapping("/{id}")
+    public ResponseEntity<Trabalho> getTrabalho(@PathVariable("id") int id){
+        Trabalho trabalho = trabalhoList.get(id);
 
-        return trabalho;
+         if (trabalho == null) {
+            return ResponseEntity.noContent().build();
+        }
+        //early return
+        return ResponseEntity.ok(trabalho);
     }
 
     @GetMapping
-    public List<Trabalho> getTrabalhoList(){
-        return trabalhoList;
+    public ResponseEntity<List<Trabalho>> getTrabalhoList(){
+        if (trabalhoList.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(trabalhoList);
     }
 
     @PostMapping
-    public void addTrabalho(@RequestBody Trabalho trabalho){
+    public ResponseEntity<Trabalho> addTrabalho(@RequestBody Trabalho trabalho){
         System.out.println("Gravou o trabalho: " +trabalho.toString());
         trabalhoList.add(trabalho);
+
+        URI url = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(trabalho.getId()).toUri();
+        return ResponseEntity.created(url).body(trabalho);
     }
+
+    @PutMapping
+    public ResponseEntity<List<Trabalho>> updateTrabalho(@PathVariable("id") int id, @RequestBody Trabalho novoTrabalho){
+        Trabalho trabalho = trabalhoList.remove(id);
+        trabalhoList.add(novoTrabalho);
+
+        return ResponseEntity.ok(trabalhoList);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> remove(@PathVariable("id") Long id) {
+        try {
+            trabalhoList.remove(id);
+            return ResponseEntity.ok("Dados Deletados");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Dados nao pode ser removidos" + e.getMessage());
+        }
+    }
+
 }
